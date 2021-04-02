9d7996f4ae943eed8071e0aa8c8dc226278e3c32
Sinitsina <65966649+Sinitsina@users.noreply.github.com>
2021-04-02 12:33:54 +0500

    All at once

--- src/main/java/org/fitnesstracker/FitnessTracker/exceptions/BadRequestException.java
+++
@@ -1,8 +0,0 @@
-package org.fitnesstracker.FitnessTracker.exceptions;
-
-public class BadRequestException extends RuntimeException {
-
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
