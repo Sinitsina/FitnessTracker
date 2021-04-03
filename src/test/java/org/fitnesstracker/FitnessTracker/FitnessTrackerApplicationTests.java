2516ff2502848ef01cfbeffa715d6fa5cf6ccb6c
Sinitsina <65966649+Sinitsina@users.noreply.github.com>
2021-04-03 21:37:06 +0500

    All at once

--- src/test/java/org/fitnesstracker/FitnessTracker/FitnessTrackerApplicationTests.java
+++ src/test/java/org/fitnesstracker/FitnessTracker/FitnessTrackerApplicationTests.java
@@ -1,12 +1,8 @@
 package org.fitnesstracker.FitnessTracker;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FitnessTrackerApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
